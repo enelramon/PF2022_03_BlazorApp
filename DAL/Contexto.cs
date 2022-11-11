f604c69595e8a99246a828554b15a62228ec316f
yeuri <yeuri@YEURI>
2022-11-11 14:22:16 -0400

    Estableciendo la Conexion con la Base de Dato y Creando el Contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PF2022_03_BlazorApp.Models;
+
+namespace PF2022_03_BlazorApp.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Tikets> tikets { get; set; }
+
+        public Contexto(DbContextOptions<Contexto> options)
+       : base(options)
+        {
+        }
+
+    }
+}
