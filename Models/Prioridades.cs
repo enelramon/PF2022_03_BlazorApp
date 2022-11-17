ece7a8bd6890878b8b262b33886e4c7bc6df0aa9
Oly Lopez Saldivar <oly_lopez@ucne.edu.do>
2022-11-16 23:11:06 -0400

    Agregando Prioridades

---
+++ Models/Prioridades.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PF2022_03_BlazorApp.Models
+{
+    public class Prioridades
+    {
+        [Key]
+        public int PrioridadId { get; set; }
+
+        public string? Descripcion { get; set; }
+
+        public string? Orden { get; set; }
+    }
+}
+
