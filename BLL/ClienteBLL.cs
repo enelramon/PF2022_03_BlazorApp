379f559527bc98a26dab1517c65c8ecefa8eb701
yeuri <yeuri@YEURI>
2022-11-17 21:34:56 -0400

    Modificando el Menu

--- BLL/ClienteBLL.cs
+++ BLL/ClienteBLL.cs
@@ -1,4 +1,4 @@
-﻿ using System.Linq.Expressions;
+﻿using System.Linq.Expressions;
 using Microsoft.EntityFrameworkCore;
 using PF2022_03_BlazorApp.DAL;
 using PF2022_03_BlazorApp.Models;
@@ -11,51 +11,53 @@ namespace PF2022_03_BlazorApp.BLL
         private Contexto _contexto;
 
         public ClienteBLL(Contexto contexto)
-    {
-        _contexto = contexto;
-    }
-
-    public bool Existe(int clienteId)
         {
-            return  _contexto.Cliente.Any(o => o.ClienteId == clienteId);
+            _contexto = contexto;
         }
-    public bool Guardar(Cliente cliente)
-          {
-               if (!Existe(cliente.ClienteId))
-                    return  this.Insertar(cliente);
-               else
-                    return  this.Modificar(cliente);
-          }
 
-    private  bool Insertar(Cliente cliente)
-    {
-        _contexto.Cliente.Add(cliente);
-        int cantidad =  _contexto.SaveChanges();
-        return cantidad > 0;
-    }
+        public bool Existe(int clienteId)
+        {
+            return _contexto.Cliente.Any(o => o.ClienteId == clienteId);
+        }
+        public bool Guardar(Cliente cliente)
+        {
+            if (!Existe(cliente.ClienteId))
+                return this.Insertar(cliente);
+            else
+                return this.Modificar(cliente);
+        }
 
-    private bool Modificar(Cliente cliente)
-    {
-        _contexto.Entry(cliente).State = EntityState.Modified;
-        return  _contexto.SaveChanges() > 0;
-    }
+        private bool Insertar(Cliente cliente)
+        {
+            _contexto.Cliente.Add(cliente);
+            int cantidad = _contexto.SaveChanges();
+            return cantidad > 0;
+        }
 
-    public bool Eliminar(Cliente cliente){
-        _contexto.Entry(cliente).State = EntityState.Deleted;
-        return   _contexto.SaveChanges() > 0;
-     }
+        private bool Modificar(Cliente cliente)
+        {
+            _contexto.Entry(cliente).State = EntityState.Modified;
+            return _contexto.SaveChanges() > 0;
+        }
 
-    public  Cliente? Buscar(int clienteId)
-    {
-        return _contexto.Cliente
-         .Where(o=> o.ClienteId == clienteId)
-        .AsNoTracking()
-        .SingleOrDefault();
-          
-    }
+        public bool Eliminar(Cliente cliente)
+        {
+            _contexto.Entry(cliente).State = EntityState.Deleted;
+            return _contexto.SaveChanges() > 0;
+        }
 
-     public  List<Cliente> GetClientes(Expression<Func<Cliente, bool>> Criterio){
-            return  _contexto.Cliente
+        public Cliente? Buscar(int clienteId)
+        {
+            return _contexto.Cliente
+             .Where(o => o.ClienteId == clienteId)
+            .AsNoTracking()
+            .SingleOrDefault();
+
+        }
+
+        public List<Cliente> GetClientes(Expression<Func<Cliente, bool>> Criterio)
+        {
+            return _contexto.Cliente
                 .AsNoTracking()
                 .Where(Criterio)
                 .ToList();
