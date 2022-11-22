1257c6a80bfb37970d19bf21bf0eb10a0a34cdc5
Oly Lopez Saldivar <oly_lopez@ucne.edu.do>
2022-11-21 23:30:03 -0400

    Arreglando Prioridades

--- Models/Prioridades.cs
+++ Models/Prioridades.cs
@@ -7,8 +7,10 @@ namespace PF2022_03_BlazorApp.Models
         [Key]
         public int PrioridadId { get; set; }
 
+        [Required(ErrorMessage = "Este campo debe completarse")]
         public string? Descripcion { get; set; }
 
+        [Required(ErrorMessage ="Este campo debe completarse")]
         public string? Orden { get; set; }
     }
 }
