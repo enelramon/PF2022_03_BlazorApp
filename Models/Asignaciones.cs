9bab5cfdc7e69b3751dc192730cf8b8f037a463e
Oly Lopez Saldivar <oly_lopez@ucne.edu.do>
2022-11-28 12:35:21 -0400

    Subiendo Registro Asignados

---
+++ Models/Asignaciones.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PF2022_03_BlazorApp.Models
+{
+    public class Asignaciones
+    {
+        [Key]
+        public int AsignacionId { get; set; }
+
+        [Required(ErrorMessage = "Este campoes requerido")]
+        public int TecnicoId { get; set; }
+
+        [Required(ErrorMessage = "Este campoes requerido")]
+        public int OrdenId { get; set; }
+
+        [Required(ErrorMessage = "Este campoes requerido")]
+        public int TicketId { get; set; }
+
+
+    }
+}
