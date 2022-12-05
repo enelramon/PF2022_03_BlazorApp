ef7a8b14593d1bca16cac43d2eed937ef3367ce5
Joseph792 <97407858+Joseph792@users.noreply.github.com>
2022-12-05 13:29:58 -0400

    Arreglos de Tipos BLL y su Registro

--- BLL/TiposBLL.cs
+++ BLL/TiposBLL.cs
@@ -16,52 +16,51 @@ namespace PF2022_03_BlazorApp.BLL
             _contexto = contexto;
         }
 
-        public bool Existe(int tipoId)
+        public async Task<bool> Existe(int tipoId)
         {
-            return _contexto.Tipos.Any(o => o.TipoId == tipoId);
+            return await _contexto.Tipos.AnyAsync(o => o.TipoId == tipoId);
         }
-        public bool Guardar(Tipos tipo)
+        public async Task<bool> Guardar(Tipos tipo)
         {
-            if (!Existe(tipo.TipoId))
-                return this.Insertar(tipo);
+            if (!await Existe(tipo.TipoId))
+                return await this.Insertar(tipo);
             else
-                return this.Modificar(tipo);
+                return await this.Modificar(tipo);
         }
 
-        private bool Insertar(Tipos tipo)
+        public async Task<bool> Insertar(Tipos tipo)
         {
             _contexto.Tipos.Add(tipo);
-            int cantidad = _contexto.SaveChanges();
-            return cantidad > 0;
+            return await _contexto.SaveChangesAsync() > 0;
         }
 
-        private bool Modificar(Tipos tipo)
+        private async Task<bool> Modificar(Tipos tipo)
         {
             _contexto.Entry(tipo).State = EntityState.Modified;
-            return _contexto.SaveChanges() > 0;
+            return await _contexto.SaveChangesAsync() > 0;
         }
 
-        public bool Eliminar(Tipos tipo)
+        public async Task<bool> Eliminar(Tipos tipo)
         {
             _contexto.Entry(tipo).State = EntityState.Deleted;
-            return _contexto.SaveChanges() > 0;
+            return await _contexto.SaveChangesAsync() > 0;
         }
 
-        public Tipos? Buscar(int tipoId)
+        public async Task<Tipos?> Buscar(int tipoId)
         {
-            return _contexto.Tipos
-             .Where(o => o.TipoId == tipoId)
+            return await _contexto.Tipos
+            .Where(o => o.TipoId == tipoId)
             .AsNoTracking()
-            .SingleOrDefault();
+            .SingleOrDefaultAsync();
 
         }
 
-        public List<Tipos> GetTipos(Expression<Func<Tipos, bool>> Criterio)
+        public async Task<List<Tipos>> GetTipos(Expression<Func<Tipos, bool>> Criterio)
         {
-            return _contexto.Tipos
+            return await _contexto.Tipos
                 .AsNoTracking()
                 .Where(Criterio)
-                .ToList();
+                .ToListAsync();
         }
     }
 }
